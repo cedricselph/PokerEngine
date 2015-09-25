9b3a361593fc7ce3592b7dfb81bbab3b90ad3631
Cedric Selph <selphc@udel.edu>
2015-09-24 20:26:51 -0400

    Update_2

--- src/Main/Card.java
+++ src/Main/Card.java
@@ -1,8 +1,34 @@
 package Main;
 
 public class Card {
-	private int rank{
+	
+	//Data Fields
+	private int rank;
+	private String suit;
+		//public static void main(String[] args){
 		
+		// Creates Card data fields
+		//String[] suit={"Spades","Hearts","Diamonds","Club"};
+		//String[] rank={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};}
+	
+	// no arg constructor
+	public Card(){}
+	
+	// method for card using rank and suit
+	public Card(int rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
 	}
+	
+	// getter for rank
+	public int getRank() {
+		return rank;
+	}
+	
+	// getter for suit
+	public String getSuit() {
+		return suit;
+	}
+			
 
 }
--- src/Main/Deck.java
+++ src/Main/Deck.java
@@ -1,11 +1,34 @@
 package Main;
 
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck extends Card{
+	
+	private ArrayList<Card> Deck_List;
+	private ArrayList<Card> Used_List = new ArrayList<Card>();
+	
+	public Deck(){
+		Deck_List= new ArrayList<Card>();
+		
+		String[] suit={"Spades","Hearts","Diamonds","Club"};
+		String[] rank={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+		
+	}
 	
 	
 	
 	
 	
-}
+	
+	
+	
+	//public static void main(String[] args){
+
+	// Creates Card data fields
+	}
+	
+	
+	
+	
+
 	
